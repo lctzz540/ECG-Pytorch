03f84196dbc36227617e6d707d2d944aa5a971c9
Thang Le <lctzz540@outlook.com>
2023-02-26 17:17:11 +0700

    some changes

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
 from ECGDataset import ECGDataset
 
-train_dataset = ECGDataset(file_path="path/to/ECG5000_TRAIN.arff")
-test_dataset = ECGDataset(file_path="path/to/ECG5000_TEST.arff")
+train_dataset = ECGDataset(file_path="dataset/train/ECG5000_TRAIN.arff")
+test_dataset = ECGDataset(file_path="dataset/test/ECG5000_TEST.arff")
